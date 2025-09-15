561f9521374faa730c6c3dc445854ff5fd77ffcb
s-sharma7 <shubhamsharmadev9@gmail.com>
2025-09-15 21:32:54 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import ConferenceEvent from './ConferenceEvent.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App>
+      <ConferenceEvent>
+      {/* <App /> */}
+      </ConferenceEvent>
+      </App>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
